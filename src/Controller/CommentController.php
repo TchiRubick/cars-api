7170a37005f97b779de343bc52264e65cbf2de8d
Tchi <tchi.devica@gmail.com>
2022-01-19 14:28:38 +0100

    refacto: format code

--- src/Controller/CommentController.php
+++ src/Controller/CommentController.php
@@ -6,21 +6,28 @@ use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\JsonResponse;
 use Symfony\Component\Routing\Annotation\Route;
 use App\Repository\CommentRepository;
+use App\Repository\CarRepository;
 
 class CommentController extends AbstractController
 {
     /**
      * @Route("/api/car/comments/{id}", name="car_comments", methods={"GET"})
      */
-    public function commentsCar(CommentRepository $commentRepository, $id): JsonResponse
+    public function commentsCar(CommentRepository $commentRepository, CarRepository $carRepository, $id): JsonResponse
     {
         $comments = $commentRepository->findBy(['car' => $id]);
         $contents = [];
         
         foreach ($comments as $comment) {
-            $contents[] = ['comment' => $comment->getContent(), 'user' => $comment->getUser()->getUsername(), 'id' => $comment->getId()];
+            $contents[] = [
+                'comment' => $comment->getContent(),
+                'user' => $comment->getUser()->getUsername(),
+                'id' => $comment->getId(),
+            ];
         }
 
-        return new JsonResponse($contents);
+        $car = $carRepository->find($id);
+
+        return new JsonResponse(['contents' => $contents, 'car' => $car->getName()]);
     }
 }
